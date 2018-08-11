d345a0b9024889e64c863885f9f4eef7c362dca4
Will Howard <willh91@msn.com>
2018-08-10 18:11:34 -0600

    pre-dinner commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello React!</div>
+    );
+  }
+}
+
+export default App;
